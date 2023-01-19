ce77e449db3f5d9d7e1eda76e8f10387fb51c402
Camila Rossi <116846050+camilahrossii@users.noreply.github.com>
2023-01-19 17:22:20 -0300

    add files

---
+++ assets/etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 12345,
+                nome: 'Daniel Silva',
+                partido: 'Algum',
+                fotos: [
+                    {url: './images/Daniel.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 54321,
+                nome: 'Erica Cristina',
+                partido: 'ABCD',
+                fotos:[
+                    {url:'./images/Erica.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 67891,
+                nome: 'Olivia Oliveira',
+                partido: 'EFGH',
+                fotos:[
+                    {url:'./images/Olivia.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 12,
+                nome: 'Ana da Silva',
+                partido: 'ABC',
+                vice: 'Henrique',
+                fotos:[
+                    {url:'./images/Ana.jpg', legenda: 'Prefeito'},
+                    {url:'./images/Henrique.jpg', legenda: 'Vice-Prefeito', small: true},
+                ]
+            },
+            {
+                numero: 34,
+                nome: 'Luis José',
+                partido: 'DEFG',
+                vice: 'Marta',
+                fotos:[
+                    {url:'./images/Luis.jpg', legenda: 'Prefeito'},
+                    {url:'./images/Marta.jpg', legenda: 'Vice-Prefeito', small:true},
+                ]
+            }
+        ]
+    },
+]
